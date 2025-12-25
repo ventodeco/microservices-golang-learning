4ff242d27dfb8c65d38c12ae4b5dd0a91d4f3f16
Yosep Novento Nugroho <yosepnoventon@gmail.com>
2025-12-25 16:02:16 +0700

    [Vento] add broker service to connect fe to be

---
+++ broker-service/cmd/api/handlers.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type jsonResponse struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+}
+
+func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
+
+	log.Println("Received request that hit the broker")
+
+	payload := jsonResponse{
+		Error:   false,
+		Message: "Hit the broker",
+	}
+
+	out, _ := json.MarshalIndent(payload, "", "\t")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusAccepted)
+	w.Write(out)
+}
---
+++ broker-service/go.mod
@@ -0,0 +1,8 @@
+module broker
+
+go 1.25.5
+
+require (
+	github.com/go-chi/chi/v5 v5.2.3 // indirect
+	github.com/go-chi/cors v1.2.2 // indirect
+)
